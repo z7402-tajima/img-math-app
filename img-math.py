1d385a2db12900f3e0ff0c81722f2d6856f12e41
z7402-tajima <g211103369@gmail.com>
2025-03-25 18:43:00 +0900

    end_0325: アプリ初版完成

--- img-math.py
+++ img-math.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 app.secret_key = "53CR3T" # シークレットキー
 
 # 定数定義
-num_classes = ["０","１","２","３","４","５","６","７","８","９"]
+num_classes = ["0","1","2","3","4","5","6","7","8","9"]
 ope_classes = ["＋","－","×","÷"] # 0:＋, 1:－, 2:×, 3:÷
 image_size = 28
-img_list = ["img1", "img2", "img3"]
+img_list = ["img1", "img2", "img3", "img4", "img5"]
 rdo_prefix = "rdo_"
 upload_folder = "upload" # アップロード画像を一時的に保持するフォルダ名
 allowed_extensions = set(["png", "jpg", "jpeg", "gif"]) # アップロードを許可する拡張子
@@ -30,7 +30,7 @@ def upload_file():
     # 値：0～9 (演算子の場合、0:＋, 1:－, 2:×, 3:÷)
     pred_list = []
 
-    # 画像送信時
+    # フォーム送信時(POST method)
     if request.method == "POST":
         i = 0 # インデックス
         pred_answer = ""
@@ -90,10 +90,10 @@ def upload_file():
 
         # 計算処理を実施
         cal = calculate(pred_list)
-        answer = pred_answer + "= " + cal
-
-    else: # GET method
-        answer += "ここに計算結果が表示されます"
+        answer = pred_answer + cal
+    # GET method
+    else:
+        answer += "ここに計算結果が表示されます（小数点以下は四捨五入）"
 
     return render_template("index.html",answer=answer)
 
@@ -118,34 +118,83 @@ def calculate(pred_list):
                 isDupFlg = False # 重複フラグをオフ
             else:
                 if isDupFlg:
-                    # 演算子が連続で設定されている場合（先頭にある場合も連続とみなす）
-                    raise ValueError("演算子が先頭もしくは連続で設定されています")
+                    # 演算子が先頭もしくは連続で設定されている場合
+                    raise ValueError(" : 計算式が正しくありません")
                 else:
                     num_list.append(num)
                     ope_list.append(pred[1])
                     num = 0
                 isDupFlg = True # 重複フラグをオン
-
         # 末尾が演算子でない場合、最後の数値をリストに加える
         if not isDupFlg:
             num_list.append(num)
 
         # 数字と演算子のリスト要素数の整合性チェック
         if len(num_list) - len(ope_list) != 1:
-            raise ValueError("式が成立していません")
-        else:
-            i = 0
-            j = 0
-            # Todo
+            raise ValueError(" : 計算式が正しくありません")
+
+        ## --ここから計算処理-- ##
+        i = 0 # 演算子インデックス
+        sum = [num_list[i]] # 左辺（最初の数字）と右辺（のちに代入）
+
+        # 演算子を順番に取り出し、四則演算を行う
+        while len(ope_list) > i:
+            j = i + 1 # 数字インデックス
+
+            match ope_list[i]:
+                # 加算(＋)
+                case 0:
+                    # 右辺が空であれば、右辺に代入
+                    if len(sum) == 1:
+                        sum.append(num_list[j])
+                    # 右辺が空でなければ、左辺と右辺を加算してから右辺に代入
+                    else:
+                        sum[0] = sum[0] + sum[1]
+                        sum[1] = num_list[j]
+                # 減算(－)
+                case 1:
+                    # 右辺が空であれば、右辺にマイナスした値を代入
+                    if len(sum) == 1:
+                        sum.append(-num_list[j])
+                    # 右辺が空でなければ、左辺と右辺を加算してから右辺にマイナスした値を代入
+                    else:
+                        sum[0] = sum[0] + sum[1]
+                        sum[1] = -num_list[j]
+                # 乗算(×)
+                case 2:
+                    # 右辺が空であれば、左辺に対して乗算
+                    if len(sum) == 1:
+                        sum[0] = sum[0] * num_list[j]
+                    # 右辺が空でなければ、右辺に対して乗算
+                    else:
+                        sum[1] = sum[1] * num_list[j]
+                # 除算(÷)
+                case 3:
+                    # 右辺が空であれば、左辺に対して除算
+                    if len(sum) == 1:
+                        sum[0] = sum[0] / num_list[j]
+                    # 右辺が空でなければ、右辺に対して除算
+                    else:
+                        sum[1] = sum[1] / num_list[j]
+                # 例外処理
+                case _:
+                    raise ValueError(" : 不正な演算子です")
+            # インデックス加算
+            i = i + 1
+        # 最後に右辺が空でなければ、左辺と右辺を加算
+        if len(sum) == 2:
+            sum[0] = sum[0] + sum[1]
+        ## --ここまで計算処理-- ##
+
+        # 計算結果をセット
+        answer = "= " + f"{sum[0]:.0f}" # 小数点以下を四捨五入
     except ZeroDivisionError as e:
-        answer = "ゼロ除算があります"
+        answer = " : ゼロ除算です"
     except ValueError as e:
         answer = str(e)
-    # 汎用例外処理は行わない（開発時のみチェック）
+    # 汎用例外処理（ローカル実行時）
     #except Exception as e:
     #    answer = str(e)
-    else:
-        answer = "Todo"
     finally:
         return answer
 
